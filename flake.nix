def8abf64af36f1d61e3fc770fefffa3a0413461
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2025-08-25 12:16:37 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "llama.cpp built with NVIDIA CUDA support";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.llama-cpp-cuda;
+
+    packages.x86_64-linux.llama-cpp-cuda = nixpkgs.legacyPackages.x86_64-linux.llama-cpp.override {
+      cudaSupport = true;
+    };
+
+  };
+}
